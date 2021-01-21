d5c23c73ca4f5e4af0ad634d318bb7c79e217b56
Carlos Damiao <carlos@verbz.ai>
2021-01-21 00:08:42 +0000

    Retrieve delivers empty on empty cache

--- Tests/FeedStoreIntegrationTests.swift
+++ Tests/FeedStoreIntegrationTests.swift
@@ -28,9 +28,9 @@ class FeedStoreIntegrationTests: XCTestCase {
 	}
 	
 	func test_retrieve_deliversEmptyOnEmptyCache() {
-		//        let sut = makeSUT()
-		//
-		//        expect(sut, toRetrieve: .empty)
+		let sut = makeSUT()
+
+		expect(sut, toRetrieve: .empty)
 	}
 	
 	func test_retrieve_deliversFeedInsertedOnAnotherInstance() {
